9633addbf7c83376a1f60fe7d5735a3d10243fd4
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2017-01-26 18:26:48 +0100

    first 3 problems

---
+++ src/main/scala/eu/atbk/euler/p00/P0001.scala
@@ -0,0 +1,10 @@
+package eu.atbk.euler.p00
+
+object P0001 {
+  def main(args: Array[String]) {
+    val x = (1 until 1000).filter(x => (x % 3 == 0) || (x % 5 == 0)).sum
+
+    println(x)
+  }
+
+}
---
+++ src/main/scala/eu/atbk/euler/p00/P0002.scala
@@ -0,0 +1,18 @@
+package eu.atbk.euler.p00
+
+import scala.BigInt
+import scala.math.BigInt.int2bigInt
+
+object P0002 {
+  def main(args: Array[String]) {
+
+    val start = (BigInt(1), BigInt(1))
+    val x = Stream.iterate(start) { case (a, b) => (b, a + b) }
+      .map { case (a, b) => b }
+      .filter { _ % 2 == 0 }
+      .takeWhile(_ < 4 * 1000 * 1000)
+      .sum
+
+    println(x)
+  }
+}
