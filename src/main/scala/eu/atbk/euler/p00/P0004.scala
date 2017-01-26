c3dd651e4c019d34908d5470003f483a3f071d7d
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2017-01-26 19:23:54 +0100

    p4

---
+++ src/main/scala/eu/atbk/euler/p00/P0004.scala
@@ -0,0 +1,29 @@
+package eu.atbk.euler.p00
+
+object P0004 {
+  def main(args: Array[String]) {
+
+    println(isPalindrome(9009))
+    println(isPalindrome(9019))
+
+    val out = countDown.takeWhile(_ > 99).flatMap(limit => countDown.takeWhile(_ >= limit).map { (limit, _) })
+      .map {
+        case (a, b) =>
+          print((a, b))
+          val p = a * b
+          println(p)
+          p
+      }
+      .filter(isPalindrome).max
+
+    println(out)
+  }
+
+  def isPalindrome(number: Long): Boolean = {
+    val digits = number.toString().toSeq
+
+    digits == digits.reverse
+  }
+
+  def countDown = Stream.iterate(999l)(_ - 1)
+}
