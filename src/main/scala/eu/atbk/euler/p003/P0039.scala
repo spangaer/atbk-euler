1c5834ecdfc2c75bff8bd63f59d336e3f45e3460
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2017-02-10 10:20:33 +0100

    P39 solved

---
+++ src/main/scala/eu/atbk/euler/p003/P0039.scala
@@ -0,0 +1,43 @@
+package eu.atbk.euler.p003
+
+import scala.collection.mutable.Buffer
+import scala.collection.immutable.{ Seq => ISeq }
+
+object P0039 {
+  def main(args: Array[String]) {
+
+    println(findSols(120))
+
+    val sol = (12 to 1000).map { i =>
+      val out = findSols(i)
+      println(s"$i\t$out")
+      out
+    }.maxBy(_.size)
+
+    println(sol)
+    println(sol.size)
+    val (a, b, c) = sol(0)
+    println(a + b + c)
+
+  }
+
+  def findSols(i: Int): Seq[(Int, Int, Int)] = {
+    val buffer = Buffer.empty[(Int, Int, Int)]
+
+    val aRange = i / 3
+
+    for (a <- (1 to aRange)) {
+      val bRange = (i - a) / 2
+      for (b <- (a to bRange)) {
+        val c = (i - a - b)
+
+        if (a * a + b * b == c * c) {
+          buffer += ((a, b, c))
+        }
+      }
+    }
+
+    buffer.to[ISeq]
+  }
+
+}
